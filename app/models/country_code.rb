2a684806cd3a8a29b68c497f95aa4af29afc858d
Hani Elabed <hani.elabed@gmail.com>
2009-01-25 00:26:56 -0600

    added test to fix nil pointer exception when country name is not found. This was because the country code for serbia is not correct in the country_codes table. It needed to be 'rs' instead of 'cs'. So I corrected it in the country_codes table.

--- app/models/country_code.rb
+++ app/models/country_code.rb
@@ -2,7 +2,7 @@ class CountryCode < ActiveRecord::Base
   
   def self.find_country( city_row )
     country_row = CountryCode.find(:first, :conditions => { :code => city_row[:country].upcase })
-    country_row.name
+    country_row.name if country_row != nil
   end
 
 end
